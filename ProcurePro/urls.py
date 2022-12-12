553e5c3136941d38600296b796374094a8e80834
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-12 10:59:50 +0200

    update urls

--- ProcurePro/urls.py
+++ ProcurePro/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from ProcurePro import core
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('core.urls'))
